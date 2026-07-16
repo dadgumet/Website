198f2559cc5a1cfde91785f896d7f72a0843cd34
dadgumet <dadgumet@gmail.com>
2026-07-16 12:57:29 -0400

    Going One Page
    Turning into a simple one page website

--- videos-data.js
+++ videos-data.js
@@ -3,78 +3,19 @@ const videosData = {
     'Season 1': {
         emoji: '🌱',
         categories: {
-            'Love': {
-                emoji: '❤️',
+            'Episode 1': {
+                emoji: '💠',
                 videos: [
-                    { id: 'love1', title: 'Love 1 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A01ALove_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A01ALove_d.mp4' },
-                    { id: 'love2', title: 'Love 2 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A01BLove_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A01BLove_d.mp4' },
-                    { id: 'love3', title: 'Love 3 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A02ALove_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A02ALove_d.mp4' },
-                    { id: 'love4', title: 'Love 4 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A02BLove_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A02BLove_d.mp4' },
-                    { id: 'love5', title: 'Love 5 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A03ALove_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A03ALove_d.mp4' },
-                    { id: 'love6', title: 'Love 6 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A03BLove_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A03BLove_d.mp4' },
-                    { id: 'love7', title: 'Love 7 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A04ALove_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A04ALove_d.mp4' },
-                    { id: 'love8', title: 'Love 8 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A04BLove_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A04BLove_d.mp4' }
-                ]
+                    { id: 'ep1', title: 'The Arrival', stream: 'https://assets.biblebarnyard.com/series/MisfitGrotto/01_s.mp4', download: 'https://assets.biblebarnyard.com/series/MisfitGrotto/01_d.mp4' }
+                                    ]
             },
-            'Truth': {
-                emoji: '✨',
-                videos: [
-                    { id: 'truth1', title: 'Truth 1 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A05ATruth_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A05ATruth_d.mp4' },
-                    { id: 'truth2', title: 'Truth 2 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A05BTruth_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A05BTruth_d.mp4' },
-                    { id: 'truth3', title: 'Truth 3 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A06ATruth_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A06ATruth_d.mp4' },
-                    { id: 'truth4', title: 'Truth 4 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A06BTruth_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A06BTruth_d.mp4' },
-                    { id: 'truth5', title: 'Truth 5 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A07ATruth_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A07ATruth_d.mp4' },
-                    { id: 'truth6', title: 'Truth 6 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A07BTruth_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A07BTruth_d.mp4' },
-                    { id: 'truth7', title: 'Truth 7 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A08ATruth_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A08ATruth_d.mp4' },
-                    { id: 'truth8', title: 'Truth 8 of 8', stream: 'https://assets.biblebarnyard.com/series/A/A08BTruth_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A08BTruth_d.mp4' }
-                ]
-            },
-            'Joy & Parents': {
-                emoji: '😊',
-                subcategories: {
-                    'Joy': {
-                        emoji: '😊',
-                        videos: [
-                            { id: 'joy1', title: 'Joy 1 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A09AJoy_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A09AJoy_d.mp4' },
-                            { id: 'joy2', title: 'Joy 2 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A09BJoy_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A09BJoy_d.mp4' },
-                            { id: 'joy3', title: 'Joy 3 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A10AJoy_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A10AJoy_d.mp4' },
-                            { id: 'joy4', title: 'Joy 4 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A10BJoy_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A10BJoy_d.mp4' }
-                        ]
-                    },
-                    'Parents': {
-                        emoji: '👨‍👩‍👧‍👦',
-                        videos: [
-                            { id: 'parents1', title: 'Parents 1 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A11AParents_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A11AParents_d.mp4' },
-                            { id: 'parents2', title: 'Parents 2 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A11BParents_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A11BParents_d.mp4' },
-                            { id: 'parents3', title: 'Parents 3 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A12AParents_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A12AParents_d.mp4' },
-                            { id: 'parents4', title: 'Parents 4 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A12BParents_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A12BParents_d.mp4' }
-                        ]
-                    }
-                }
-            },
-            'Lying & Why Bad Things Happen': {
-                emoji: '🤥',
-                subcategories: {
-                    'Lying': {
-                        emoji: '🤥',
-                        videos: [
-                            { id: 'lying1', title: 'Lying 1 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A13ALying_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A13ALying_d.mp4' },
-                            { id: 'lying2', title: 'Lying 2 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A13BLying_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A13BLying_d.mp4' },
-                            { id: 'lying3', title: 'Lying 3 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A14ALying_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A14ALying_d.mp4' },
-                            { id: 'lying4', title: 'Lying 4 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A14BLying_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A14BLying_d.mp4' }
-                        ]
-                    },
-                    'Why Bad Things Happen': {
-                        emoji: '❓',
-                        videos: [
-                            { id: 'whybad1', title: 'Why Bad Things Happen 1 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A15ABadThings_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A15ABadThings_d.mp4' },
-                            { id: 'whybad2', title: 'Why Bad Things Happen 2 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A15BBadThings_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A15BBadThings_d.mp4' },
-                            { id: 'whybad3', title: 'Why Bad Things Happen 3 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A16ABadThings_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A16ABadThings_d.mp4' },
-                            // { id: 'whybad4', title: 'Why Bad Things Happen 4 of 4', stream: 'https://assets.biblebarnyard.com/series/A/A16BBadThings_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A16BBadThings_d.mp4' }
-                        ]
-                    }
-                }
-            }
+            // 'Episode 2': {
+            //     emoji: '💠',
+            //     videos: [
+            //         { id: 'truth1', title: 'A New Friend', stream: 'https://assets.biblebarnyard.com/series/A/A05ATruth_s.mp4', download: 'https://assets.biblebarnyard.com/series/A/A05ATruth_d.mp4' },
+            //                        ]
+           
+            // }
         }
     }
 };
